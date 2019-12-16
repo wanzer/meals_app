59452b4a7ebd6df49343de26622fc5ed995ff4fa
Dmitryi Savitskyi <dmitryi.savitskyi@gmail.com>
2019-12-16 20:59:38 +0200

    bottom nav bar

--- lib/screens/tabsScreen.dart
+++ lib/screens/tabsScreen.dart
@@ -8,8 +8,7 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  @override
-  Widget build(BuildContext context) {
+  Widget setTablayout() {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -22,10 +21,50 @@ class _TabsScreenState extends State<TabsScreen> {
             ],
           ),
         ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(),
-          FavoritesScreen()
-        ],),
+        body: TabBarView(
+          children: <Widget>[CategoriesScreen(), FavoritesScreen()],
+        ),
+      ),
+    );
+  }
+
+  List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': "Categories"},
+    {'page': FavoritesScreen(), 'title': "Favorites"}
+  ];
+
+  int _selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectPageIndex]['title']),
+      ),
+      body: _pages[_selectPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        //backgroundColor: Theme.of(context).primaryColor,
+        // unselectedItemColor: Colors.white,
+        // selectedItemColor : Theme.of(context).accentColor,
+        currentIndex: _selectPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              title: Text("Categories")),
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              title: Text("Favorites"))
+        ],
       ),
     );
   }
